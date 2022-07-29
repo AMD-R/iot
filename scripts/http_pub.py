de91135b652f33f4853c1eced7f9c17cdb6550e9
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-29 16:48:34 +0800

    Improved http_pub

--- scripts/http_pub.py
+++ scripts/http_pub.py
@@ -5,55 +5,97 @@ import random
 import datetime
 import rospy
 import os
+import sys
 from typing import NoReturn
 from cryptography_scripts import import_private, sign_message
 
 
-def start_server(host: str, port: int, private_key: str, key_password) -> (
-        NoReturn
-):
+def start_server(host: str, port: int, private_key: str, key_password: str,
+                 verbose: bool = True) -> NoReturn:
     """Starts the http client.
     Prameters
     ---------
     host: str
-    The host name of the http server
+        The host name of the http server
     port: int
-    The port number of the server
+        The port number of the server
+    private_key: str
+        The private key file name
+    key_password: str
+        The password used to encrypt the private key
+    verbose: bool
+        If True node will log the message sent to the server
     """
+    rate = rospy.Rate(1)
+
     if key_password:
         key_password = bytes(key_password, 'utf-8')
     url = host + ":" + str(port) + '/api/amd-r/subscriber'
-    data = {
-        'gps': {
-            'lat': random.random() * (2.95 - 2.94) + 2.94,
-            'lon': random.random() * (101.875 - 101.874) + 101.874,
-        },
-        'battery': random.random() * 100,
-        'speed': random.random() * (2 - 0) + 0,
-        'mission': random.random(),
-        'Time': datetime.datetime.now().isoformat()
-    }
-    message = bytes(str(data).replace(" ", "").replace("'", '"'), 'utf-8')
 
-    key = import_private(private_key, key_password)
-    signature = sign_message(key, message)
-    data['signature'] = signature.hex()
-    data['name'] = 'Test AMD-R'
+    while not rospy.is_shutdown():
+        data = {
+            'gps': {
+                'lat': random.random() * (2.95 - 2.94) + 2.94,
+                'lon': random.random() * (101.875 - 101.874) + 101.874,
+            },
+            'battery': random.random() * 100,
+            'speed': random.random() * (2 - 0) + 0,
+            'mission': random.random(),
+            'Time': datetime.datetime.now().isoformat()
+        }
+        message = bytes(str(data).replace(" ", "").replace("'", '"'), 'utf-8')
 
-    post = requests.post(url, json=data)
+        key = import_private(private_key, key_password)
+        signature = sign_message(key, message)
+        data['signature'] = signature.hex()
+        data['name'] = 'Test AMD-R'
+
+        # Logging message
+        if verbose:
+            rospy.loginfo("Sending: " + str(data))
+
+        # Sending Data
+        try:
+            post = requests.post(url, json=data)
+        except requests.ConnectionError:
+            rospy.logerr("Unable to connect to server. Please Ensure the host "
+                         "and port is correct or the server is up."
+                         "Retrying...")
+        rate.sleep()
 
 
 if __name__ == '__main__':
     # https://answers.ros.org/question/109761/where-to-download-extra-files-needed-at-runtime/
     # https://answers.ros.org/question/143281/where-should-i-put-configuration-files/
+    # Getting Parameters
     rospy.init_node("http_pub")
     config = rospy.get_param('/httppub/config')
-    with open(config, 'r') as f:
-        data = yaml.safe_load(f)
-        key_dir = os.path.dirname(config)
-        key = os.path.join(key_dir, data['private_key'])
+    verbose = rospy.get_param('/httppub/verbose', 1)
 
+    # Getting configuration
     try:
-        start_server(data['host'], data['port'], key, data['password'])
-    except KeyboardInterrupt:
+        with open(config, 'r') as f:
+            data = yaml.safe_load(f)
+            key_dir = os.path.dirname(config)
+            key = os.path.join(key_dir, data['private_key'])
+    except FileNotFoundError:
+        rospy.logerr("Could Not Find Config File")
+        sys.exit(1)
+    except KeyError:
+        rospy.logerr("Could not find private_key. Please make sure the config "
+                     "file is configure correctly")
+        sys.exit(1)
+
+    # Starting Loop
+    try:
+        start_server(data['host'], data['port'], key, data['password'],
+                     int(verbose))
+    except KeyError:
+        rospy.logerr("Invalid config file, please ensure it is configure "
+                     "correctly")
+        sys.exit(1)
+    except rospy.ROSInterruptException:
         pass
+    except ValueError:
+        start_server(data['host'], data['port'], key, data['password'],
+                     int(1))
