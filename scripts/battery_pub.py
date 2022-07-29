ae1d3bb40553130c0b6ae7aa948f682bb734ab6c
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-29 16:54:30 +0800

    Improved battery_pub

--- scripts/battery_pub.py
+++ scripts/battery_pub.py
@@ -4,19 +4,23 @@ import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import BatteryState
 
-def callback(data):
-    global pub_battery
+
+def callback(data: BatteryState, publisher: rospy.Publisher):
+    """Callback Function for battery topic."""
+    # global pub_battery
     battery_percentage = data.percentage
-    pub_battery.publish(battery_percentage)
-    
-    
+    publisher.publish(battery_percentage)
+
+
 def listener():
-    global pub_battery
+    """Publisher node for battery_percentage."""
+    # global pub_battery
     rospy.init_node('battery_tmp_sub', anonymous=True)
-    rospy.Subscriber("battery", BatteryState, callback)
     pub_battery = rospy.Publisher('battery_percentage', Float32, queue_size=10)
-    
+    rospy.Subscriber("battery", BatteryState, callback, pub_battery)
+
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
